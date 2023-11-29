bc567b1fc393746492520135287739468241af63
ColiJD <Colindresj9@gmail.com>
2023-11-29 02:10:34 +0000

    create header and footer

--- variables.js
+++ variables.js
@@ -1,7 +1,76 @@
+const navegacion = [
+	{
+		name: "Home",
+		url: "./index.html",
+	},
+	{
+		name: "Products",
+		url: "./index.html",
+	},
+	{
+		name: "About Us",
+		url: "./index.html",
+	},
+	{
+		name: "Contact Us",
+		url: "./index.html",
+	},
+];
 
+const input = [
+	{
+		type: "text",
+		name: "name",
+		id: "name",
+		placeholder: "Name",
+		required: "required",
+	},
+	{
+		type: "email",
+		name: "email",
+		id: "email",
+		placeholder: "Email",
+		required: "required",
+	},
+];
+
+
+const phones = [
+	{
+		numero: "tel:+1234567890",
+		etiqueta: "+1 234 567 890",
+	},
+	{
+		numero: "tel:+1234567890",
+		etiqueta: "+1 234 567 890",
+	},
+];
+const footerNavegacion = [
+	{
+		name: "Home",
+		url: "./index.html",
+	},
+	{
+		name: "Products",
+		url: "./index.html",
+	},
+	{
+		name: "About Us",
+		url: "./index.html",
+	},
+	{
+		name: "Contact Us",
+		url: "./index.html",
+	},
+];
 
 const handlerBarsContext = {
 	"/index.html": {
+		// title: "Bakery Landing Page",
+		navegacion: navegacion,  
+		input: input,
+		phones: phones,
+		footerNavegacion: footerNavegacion,
 	},
 };
 
--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ import handlebars from 'vite-plugin-handlebars';
 import handlerBarsContext from './variables.js';
 
 export default defineConfig({
-    base: "/examen-segundo-parcial/",
+    base: "/ProyectoPw-2/",
     appType: 'mpa',
     build: {
         rollupOptions: {
