3d0be51c6a4cc7538e8252573c682ddabad83b0e
ColiJD <Colindresj9@gmail.com>
2023-12-04 21:00:25 -0600

    Merge de KV

--- js/data.js
+++ js/data.js
@@ -47,7 +47,7 @@ export const navegacion = [
 	},
 	{
 		name: "Nuestros Programas",
-		url: "/index.html",
+		url: "/ProyectoPw-2/page/programas.html",
 	},
 	{
 		name: "Cómo Ayudar",
@@ -75,7 +75,7 @@ export const navegacion = [
 	},
 	{
 		name: "Preguntas Frecuentes (FAQ)",
-		url: "/index.html",
+		url: "/ProyectoPw-2/page/pfrecuentes.html",
 	},
 ];
 
@@ -196,5 +196,94 @@ export const generalidadesInfo = [
 		urlImg: "https://acoes.org/wp-content/uploads/2021/09/plan.png",
 		subtitulo: "Planes de Actuacións",
 		link: "#",
+	},
+];
+export const teest = [
+	{
+		url: "https://acoes.org/wp-content/uploads/2021/09/DSC_0423-OSCAR-MALAGON.jpg",
+		t1: "Proyecto Educativo",
+		t2: "Ubicacion",
+		p1: "Tegucigalpa y los departamentos de Francisco Morazán, Copán, Lempira, La Paz, Olancho, Intibucá, Valle, El Paraíso, Comayagua, Choluteca y Gracias a Dios.",
+		t3: "Descripcion",
+		p2: "Santa Mónica es un centro de día que tiene por objeto integrar familiar y socialmente, así como en procesos educativos, a niños/as y jóvenes. El centro se encuentra localizado concretamente en la colonia marginal que lleva el nombre de Villa Cristina donde las familias residentes viven en una pobreza inminente y también la criminalidad, las drogas y las pandillas juveniles (maras) tienen una enorme incidencia. La mayoría de estas familias están en un estado disfuncional en donde las que sobresalen son las madres como parte paternal. Muchas de estas personas tienen que trabajar en casas de familias con un mayor nivel económico para poder tener un medio de subsistencia. Entre los trabajos más comunes podemos mencionar el lavado y planchado de ropa ajena, aseo general de la casa, actividad que complementan con la venta de tortillas de maíz y la recolección de botellas desechables ya sea de la calle o de los contenedores de basura cercanos a la colonia. Es aquí donde el centro Santa Mónica desarrolla su labor de atención y  educación con los niños y jóvenes  de la colonia brindando a los beneficiarios refuerzo escolar, orientación personal y capacitaciones de actividades lúdicas (artes plásticas y música). Reciben un almuerzo en un turno de mañana y otro de tarde. El centro también es un espacio abierto a las madres y ancianos de la zona  donde se les ofrece asesoramiento psicológico (escuela para madres) y ayuda con donaciones de ropa y comida.",
+	},
+	{
+		url: "https://acoes.org/wp-content/uploads/2017/06/2-min-7.jpg",
+		t1: "Proyectos Asistenciales",
+		t2: "Ubicacion",
+		p1: "15 colonias de Tegucigalpa.",
+		t3: "Descripcion",
+		p2: "Como cada año desde las diferentes delegaciones de ACOES de España y desde Canadá de la mano de Canadian Food For Children, se realiza una labor de recogida de una selección de materiales en las que contribuyen parroquias, centros educativos, alcaldías y otras instituciones y familias que quieren compartir. Con el apoyo de muchos voluntarios se llenan los contenedores que son enviados con navieras a Honduras en donde se reciben y son repartidos para cubrir las necesidades de los diferentes centros del proyecto o en forma de packs de primera necesidad a las familias más necesitadas de colonias y comunidades en las que ACOES está presente. Cabe mencionar que la ayuda percibida es un soporte esencial disminuyendo los costes en comida, mobiliario y equipamiento de la organización.  Ya se han mandado más de 300 contenedores desde el año 1994 que se mandó el primero.",
+	},
+	{
+		url: "https://acoes.org/wp-content/uploads/2017/06/1-min-18.jpg",
+		t1: "Proyectos Sanitarios",
+		t2: "Ubicacion",
+		p1: "Colonia Nueva Capital",
+		t3: "Descripcion",
+		p2: "Nueva Capital  es una montaña  ocupada por damnificados de huracán Mitch e inmigración de zonas de extrema pobreza.La cobertura de salud pública de la Nueva Capital es muy limitada. Solo hay un dispensario médico en la parroquia que abre 4 horas a la semana y que necesita apoyo en medios y supervisión médica. La población es muy vulnerable a problemas de salud. Tiene un impacto directo en la educación de la población joven que es el futuro de la comunidad. Desde principios de 2016 ACOES dispone de una clínica en una de sus 4 escuelas en la zona. Equipo de 6 personas dirigido por médico y odontóloga de servicio social. Hospital Escuela de Honduras y Universidad de Málaga apoyan el proyecto. Equipo de agentes en salud en formación (estudiantes ACOES y miembros de la comunidad y Pastoral de la Salud) Para abarcar el mayor porcentaje de las 50.000 personas de población, que viven en esta zona, se han establecido 4 puntos de salud adicionales a la clínica. El proyecto de salud persigue reducir las derivaciones al sistema de salud públicos a los casos diagnosticados y patologías graves, dado el problema de acceso a los hospitales y la situación precaria de los hospitales públicos.",
+	},
+	{
+		url: "https://acoes.org/wp-content/uploads/2017/06/3-min-14.jpg",
+		t1: "Proyectos de Construcción",
+		t2: "Ubicacion",
+		p1: "Todo el pais",
+		t3: "Descripcion",
+		p2: "En el país hay una gran carencia de viviendas, por lo que algunas colaboraciones van encaminadas a apoyar a familias de escasos recursos en una construcción en condiciones que permita reducir enfermedades así como tener un lugar digno donde residir. Las familias han participado en la construcción y adquirido material de la zona por lo que los costos son menores.",
+	},
+	{
+		url: "https://acoes.org/wp-content/uploads/2017/06/1-min-14.jpg",
+		t1: "Proyectos de Productividad",
+		t2: "Ubicacion",
+		p1: "Macuelizo, Departamento de Francisco Morazán",
+		t3: "Descripcion",
+		p2: "Abastecer parte de las necesidades alimenticias de los distintos proyectos (escuelas, kínderes, proyectos Populorum, etc.), involucrando a los jóvenes colaboradores en actividades agropecuarias para el cultivo de productos, como pueden ser: granos básicos, hortalizas, frutas y una granja avícola. Además es un espacio natural que sirve para trabajar la educación ambiental con los niños/as y jóvenes de los proyectos educativos.",
+	},
+];
+
+export const preguntas = [
+	{
+		pregunta: "1. Donde Estamos Ubicados?",
+		respuesta:
+			"En todo el pais, consulte el proyecto de su interes para mas informacion",
+	},
+	{
+		pregunta: "2. Cuanto es el donativo minimo que puedo realizar?",
+		respuesta: "Menos de 1€ al día. El 100% de la ayuda llega.",
+	},
+	{
+		pregunta: "3. Cuantos proyectos tienen en marcha?",
+		respuesta: "68 proyectos, 8000 ninos becados y 55 centros educativos",
+	},
+	{
+		pregunta: "4. Donde puedo ver las cuentas anuales de la oreganizacion?",
+		respuesta:
+			"Puedes ver las cuentas anuales en https://drive.google.com/drive/folders/1Dmojz21jDVfYyQV6p2wcGoISMQO98nv6?usp=sharing",
+	},
+	{
+		pregunta: "5. Que es la ACOES?",
+		respuesta:
+			"Es una organización española que nace en 2020 con el objetivo canalizar y favorecer la corriente de solidaridad de las personas, colaborar y sostener los proyectos ejecutados en Honduras por su organización homóloga en Tegucigalpa (ACOES Honduras) y prestar ayuda y soporte a las múltiples organizaciones, grupos y amigos, que apoyan esta misión.",
+	},
+	{
+		pregunta: "6. Que consiguen los ninos que apadrino?",
+		respuesta: "Uniforme, mochila, zapatos y material escolar para ir a la escuela",
+	},
+	{
+		pregunta: "7. Donde se fundo?",
+		respuesta: "En espana",
+	},
+	{
+		pregunta: "8. Cuantas delegaciones hay?",
+		respuesta: "9 Delegaciones",
+	},
+	{
+		pregunta: "9. Como puedo ayudar aparte de donar?",
+		respuesta: "Voluntariado, Acogida de estudiantes y colaborando con la empresa",
+	},
+	{
+		pregunta: "10. Cuantos colaboradores hay trabajando?",
+		respuesta:
+			"En la actualidad son más de novecientos colaboradores hondureños los que hacen realidad los distintos proyectos con la financiación obtenida en el extranjero y en Honduras.",
 	},
 ];
--- variables.js
+++ variables.js
@@ -1,106 +1,18 @@
-const navegacion = [
-	{
-		name: "Inicio",
-		url: "./index.html",
-	},
-	{
-		name: "Acerca de Nosotros:",
-		url: "./index.html",
-	},
-	{
-		name: "Nuestros Programas",
-		url: "./index.html",
-	},
-	{
-		name: "Cómo Ayudar",
-		url: "./index.html",
-	},
-	{
-		name: "Galería de Fotos",
-		url: "./index.html",
-	},
-	{
-		name: "Blog",
-		url: "./index.html",
-	},
-	{
-		name: "Eventos",
-		url: "./index.html",
-	},
-	{
-		name: "Contáctenos",
-		url: "./index.html",
-	},
-	{
-		name: "Página de Donaciones:",
-		url: "./index.html",
-	},
-	{
-		name: "Preguntas Frecuentes (FAQ)",
-		url: "./index.html",
-	},
-];
-
-const input = [
-	{
-		type: "text",
-		name: "name",
-		id: "name",
-		placeholder: "Name",
-		required: "required",
-	},
-	{
-		type: "email",
-		name: "email",
-		id: "email",
-		placeholder: "Email",
-		required: "required",
-	},
-];
-
-const phones = [
-	{
-		numero: "tel:+1234567890",
-		etiqueta: "+1 234 567 890",
-	},
-	{
-		numero: "tel:+1234567890",
-		etiqueta: "+1 234 567 890",
-	},
-];
-const footerNavegacion = [
-	{
-		name: "Home",
-		url: "./index.html",
-	},
-	{
-		name: "Products",
-		url: "./index.html",
-	},
-	{
-		name: "About Us",
-		url: "./index.html",
-	},
-	{
-		name: "Contact Us",
-		url: "./index.html",
-	},
-];
-
-const galery = [
-	{
-		url: "https://acoes.org/wp-content/uploads/2021/09/banner-home-2.png",
-	},
-	{
-		url: "https://acoes.org/wp-content/uploads/2021/09/20210216_082641.jpg",
-	},
-	{
-		url: "https://acoes.org/wp-content/uploads/2023/01/Buena_Vista-movil.jpg",
-	},
-	{
-		url: "https://acoes.org/wp-content/uploads/2021/09/Sin-titulo-1_0000_IMG_7486_joaquin-roldan.png",
-	},
-];
+import {
+	navegacion,
+	galery,
+	generalidades,
+	generalidadesInfo,
+	input,
+	select,
+	phones,
+	footerNavegacion,
+	redes,
+	card,
+	btnApadrina,
+	teest,
+	preguntas,
+} from "./js/data";
 
 const handlerBarsContext = {
 	"/index.html": {
@@ -120,13 +32,15 @@ const handlerBarsContext = {
 		title: "Donaciones",
 		logo: "https://acoes.org/wp-content/uploads/2023/10/LG-FUNDACION-BN-2-2.png",
 		navegacion: navegacion,
-		btnApadrina:btnApadrina,
-		card:card,
+		btnApadrina: btnApadrina,
+		card: card,
 		phones: phones,
 		footerNavegacion: footerNavegacion,
 		redes: redes,
 	},
-	"/programas.html": {
+	"/page/programas.html": {
+		title: "Programas",
+		logo: "https://acoes.org/wp-content/uploads/2023/10/LG-FUNDACION-BN-2-2.png",
 		navegacion: navegacion,
 		galery: galery,
 		input: input,
@@ -134,7 +48,9 @@ const handlerBarsContext = {
 		footerNavegacion: footerNavegacion,
 		teest: teest,
 	},
-	"/pfrecuentes.html": {
+	"/page/pfrecuentes.html": {
+		title: "Frecuentes",
+		logo: "https://acoes.org/wp-content/uploads/2023/10/LG-FUNDACION-BN-2-2.png",
 		navegacion: navegacion,
 		galery: galery,
 		input: input,
